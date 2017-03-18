6db8f10559dad165d4a0097f30f4027f68b79c58
Konstantin A <kostya.dn@gmail.com>
2017-03-18 16:00:20 +0300

    Initial dev commit

---
+++ Tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Pdazcom\LaravelWalletOne\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+
+}
---
+++ Tests/WalletonePayTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pdazcom\LaravelWalletOne\Tests;
+
+class WalletonePayTest extends TestCase
+{
+    public function testHandle()
+    {
+        $this->assertTrue(true);
+    }
+}
