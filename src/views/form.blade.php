68a8a445f23c9cfc79f4828c8f2f0e0065c03947
Konstantin A <kostya.dn@gmail.com>
2017-03-25 12:51:50 +0300

    fix: form labels error

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,7 +1,11 @@
 <form id="walletone-form" method="post" action="{{ \Pdazcom\LaravelWalletOne\WalletOne::API_URL }}">
     @foreach($fields as $name => $value)
-        <label for="{{ $name }}">{{ ucfirst($name) }}:</label>
-        <input type="{{ $name === 'WMI_PAYMENT_AMOUNT' ? 'text' : 'hidden' }}" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"/>
+        @if($name === 'WMI_PAYMENT_AMOUNT')
+            <label for="{{ $name }}">{{ ucfirst($name) }}:</label>
+            <input type="text" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"/>
+            @continue
+        @endif
+        <input type="hidden" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"/>
     @endforeach
 
     <input type="submit" value="{{ config('wallet-one.buttonLabel') }}" />
